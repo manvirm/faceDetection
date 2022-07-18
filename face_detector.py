51ed324428fb22c9c977202b565bba7dc872a418
Manvir Mann <manvirm29@gmail.com>
2022-07-18 16:59:17 -0700

    find coordinates and draw rectangles around face

--- face_detector.py
+++ face_detector.py
@@ -13,13 +13,25 @@ img = cv2.imread('kreeves.jpg')
 # This function converts image to specific colors
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# Detect faces
+# No matter size of face it will detect (multi scale)
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+# Format: [[top left point, width, height]]  
+#print(face_coordinates)
+
+# Draw rectangles around the face
+# First 2 tuples are 1.top left point and 2.bottom right point of rectangle
+# 3rd tuple is color of rectangle (blue RGB in this case)
+# Last argument is the thickness of rectangle
+#cv2.rectangle(img, (top left), (222+width, 292+height),  (0, 0, 255), 2)
+#cv2.rectangle(img, (222, 292), (222+340, 292+340),  (0, 0, 255), 2)
+(x, y, w, h) = face_coordinates[0]
+
+cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
 
 # Show image
-cv2.imshow('Programming Face Detector', grayscaled_img)
+cv2.imshow('Programming Face Detector', img)
 
 # Pause code so image will show until key is pressed
 cv2.waitKey()
-
-
-
-print("code completed")
