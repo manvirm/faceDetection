5f108d57805c352b9af7bfbb535ed5e029082800
Manvir Mann <manvirm29@gmail.com>
2022-07-18 16:21:26 -0700

    grayscale image

--- face_detector.py
+++ face_detector.py
@@ -9,10 +9,17 @@ trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Image is just an array (pixels = numbers)
 img = cv2.imread('kreeves.jpg')
 
+# Must convert image to grayscale for algorithm to recognize
+# This function converts image to specific colors
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
 # Show image
-cv2.imshow('Programming Face Detector', img)
+cv2.imshow('Programming Face Detector', grayscaled_img)
 
 # Pause code so image will show until key is pressed
 cv2.waitKey()
+
+
 
 print("code completed")
